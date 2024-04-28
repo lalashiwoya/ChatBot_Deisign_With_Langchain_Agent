a32be8110c4ee8965edc2046e435e8b0cd302ed5
danL <dan-liu@web.de>
2024-04-28 19:39:54 +0100

    add url source at the end of answer

--- api/prompts/qa_prompt_with_rag.py
+++ api/prompts/qa_prompt_with_rag.py
@@ -1,6 +1,16 @@
-template = """Context: {context}\n\n
-Chat History: {memory} \n
-Answer the following question based on the context provided:\n\n
-Question: {question}\n
-Answer: \n
+template = """Context: {context}
+
+Chat History: {memory}
+
+Please respond to the question below using the information from 
+the provided context and chat history. 
+Note the requirement to append any unique URLs from the context at the end of your response. 
+These URLs serve as information source to illustrate how you derived your answer.
+
+Question: {question}
+
+Answer:
+
+
+
 """
