7d70530dab0e46d40a498adee92098f4b16931e5
danL <dan-liu@web.de>
2024-04-28 19:34:41 +0100

    move parameters to config.toml

--- api/chains/qa_chain_with_rag.py
+++ api/chains/qa_chain_with_rag.py
@@ -5,18 +5,19 @@ from operator import itemgetter
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema import StrOutputParser
 from llama_index.embeddings.openai import OpenAIEmbedding
+from api.utils import get_retriever
 
-path = "data/llm_finetune/urls/urls.txt"
-if_clean_texts = False
+# path = "data/llm_finetune/urls/urls.txt"
+# if_clean_texts = False
 
-docs = WebPagesToDocuments(path = path, clean_texts=if_clean_texts).docs
+# docs = WebPagesToDocuments(path = path, clean_texts=if_clean_texts).docs
 
-retriever = LlamaRetriever(db_path="data/llm_finetune/db",
-                           chunk_size = 360,
-                           embeddings_model=OpenAIEmbedding(model="text-embedding-3-small"),
-                           docs = docs)
+# retriever = LlamaRetriever(db_path="data/llm_finetune/db",
+#                            chunk_size = 360,
+#                            embeddings_model=OpenAIEmbedding(model="text-embedding-3-small"),
+#                            docs = docs)
 
-
+retriever = get_retriever(path = "config.toml")
 def create_llm_finetun_chain(llm):
     chain = (
         {"question": itemgetter("question"),
